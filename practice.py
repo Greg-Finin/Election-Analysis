048e3a76f4087caa6d56a5da3944b8dc25049cc9
Greg-Finin <71339960+Greg-Finin@users.noreply.github.com>
2020-09-22 17:30:19 -0500

    Delete practice.py

--- practice.py
+++
@@ -1,5 +0,0 @@
-car_list = [{"brand":"honda", "year": 2018}, {"brand":"jeep", "year": 2019}, {"brand": "Audi", "year": 2020}] 
-
-for car in car_list:
-        print(f" {car['brand']}, {car['year']}")
-
