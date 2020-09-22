65e5d830f488dda59a0c14c88a8da5175681a560
Greg-Finin <71339960+Greg-Finin@users.noreply.github.com>
2020-09-22 17:30:26 -0500

    Delete skilldrill.py

--- skilldrill.py
+++
@@ -1,5 +0,0 @@
-counties_dicto = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
-
-for counties, voters in counties_dicto.items():
-    print(f' {counties} county has {voters} voters')
-
