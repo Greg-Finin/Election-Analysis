71ca516247624c66464fe4988296df0f5eb9ab5d
Greg-Finin <71339960+Greg-Finin@users.noreply.github.com>
2020-09-22 17:30:04 -0500

    Delete new_dict.py

--- new_dict.py
+++
@@ -1,8 +0,0 @@
-counties_dict = []
-counties_dict.append({'county':'Arapahoe', 'voters': 10})
-counties_dict.append({'county':'Jefferson', 'voters': 11})
-counties_dict.append({'county':'Denver', 'voters': 12})
-
-
-for county, voters in counties_dict:
-    print(county, voters)
