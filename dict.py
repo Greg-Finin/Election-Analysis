fe0cf0ade19f6404ec43994120495c6f2aba7a38
Greg-Finin <71339960+Greg-Finin@users.noreply.github.com>
2020-09-22 17:29:32 -0500

    Delete dict.py

--- dict.py
+++
@@ -1,4 +0,0 @@
-
-counties_dict = ({'county': "Arapahoe", 'voters': 422829, 'county': "Denver", 'voters': 463353, 'county': "Jefferson", 'voters' 432438})
-for county, voters in counties_dict.items():
-    print(county, voters)
